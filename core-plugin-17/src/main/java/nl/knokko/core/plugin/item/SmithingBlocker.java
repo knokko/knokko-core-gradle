b6536803867ce28637407d76c31dcd4d22c0f583
knokko <knokogator@hotmail.com>
2022-01-25 10:02:42 +0100

    Implement blockSmithingTableUpgrades in core-plugin-17

--- core-plugin-17/src/main/java/nl/knokko/core/plugin/item/SmithingBlocker.java
+++ core-plugin-17/src/main/java/nl/knokko/core/plugin/item/SmithingBlocker.java
@@ -1,5 +1,11 @@
 package nl.knokko.core.plugin.item;
 
+import nl.knokko.core.plugin.CorePlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareSmithingEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.function.Predicate;
@@ -7,6 +13,26 @@ import java.util.function.Predicate;
 public class SmithingBlocker {
 
     public static void blockSmithingTableUpgrades(Predicate<ItemStack> shouldBeBlocked) {
-        // This minecraft version has a smithing table, but no recipes xD
+        Bukkit.getPluginManager().registerEvents(
+                new SmithingBlockEventHandler(shouldBeBlocked),
+                CorePlugin.getInstance()
+        );
+    }
+
+    private static class SmithingBlockEventHandler implements Listener {
+
+        private final Predicate<ItemStack> shouldBeBlocked;
+
+        SmithingBlockEventHandler(Predicate<ItemStack> shouldBeBlocked) {
+            this.shouldBeBlocked = shouldBeBlocked;
+        }
+
+        @EventHandler
+        public void blockSmithingTableUpgrades(PrepareSmithingEvent event) {
+            ItemStack toUpgrade = event.getInventory().getItem(0);
+            if (shouldBeBlocked.test(toUpgrade)) {
+                event.setResult(new ItemStack(Material.AIR));
+            }
+        }
     }
 }
